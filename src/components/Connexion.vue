35510ef650c06d887a34432d7f4eafce361c4b6f
Michael Devos <mdevos.mld@gmail.com>
2023-08-13 23:04:57 +0200

    upgrade connexion.vue

--- src/components/Connexion.vue
+++ src/components/Connexion.vue
@@ -1,10 +1,44 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container>
+        <v-card>
+          <v-card-title>
+            {{ isLogin ? 'Connexion' : 'Inscription' }}
+          </v-card-title>
+          <v-card-text>
+            <v-form v-if="!isLogin">
+              <v-text-field v-model="firstname" label="Prénom" required></v-text-field>
+              <v-text-field v-model="lastname" label="Nom" required></v-text-field>
+              <v-text-field v-model="email" label="Email" type='email' required></v-text-field>
+              <v-text-field v-model="password" label="Mot de passe" type="password"></v-text-field>
+              <v-btn @click="register" color="primary">S'inscrire</v-btn>
+            </v-form>
+            <v-form v-else>
+              <v-text-field v-model="loginEmail" label="Email" type="email"></v-text-field>
+              <v-text-field v-model="loginPassword" label="Mot de passe" type="password"></v-text-field>
+              <v-btn @click="login" color="primary">Se connecter</v-btn>
+            </v-form>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn @click="toggleLoginMode">
+              {{ isLogin ? "Pas encore inscrit ?" : "Déjà inscrit ?" }}
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+
 <script>
 export default {
   data() {
     return {
       isLogin: true,
-      firstName: "",
-      lastName: "",
+      firstname: "",
+      lastname: "",
       email: "",
       password: "",
       loginEmail: "",
@@ -17,23 +51,40 @@ export default {
     },
     async register() {
       try {
-        // Envoyer les données d'inscription au serveur (simulé ici avec un délai)
-        await new Promise(resolve => setTimeout(resolve, 1000)); // Simule une requête asynchrone
+        const response = await fetch('http://localhost:5000/api/register',{
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            firstname: this.firstname,
+            lastname: this.lastname,
+            email: this.email,
+            password: this.password
+          }),
+        });
 
-        // Après l'inscription réussie, vous pouvez réinitialiser les champs ou effectuer d'autres actions
-        this.firstName = "";
-        this.lastName = "";
-        this.email = "";
-        this.password = "";
+        const responseData = await response.json();
 
-        // Afficher un message de succès ou rediriger vers une autre page si nécessaire
-        alert("Inscription réussie ! Vous pouvez maintenant vous connecter.");
+        if (response.ok) {
+          // Réinitialiser les champs d'inscription après une inscription réussie
+          this.firstname = "";
+          this.lastname = "";
+          this.email = "";
+          this.password = "";
+
+          alert("Inscription réussie ! Vous pouvez maintenant vous connecter.");
+        } else {
+          // Gérer les cas où la réponse n'est pas OK (par exemple, afficher des messages d'erreur)
+          console.error('Erreur lors de l\'inscription :', responseData.message);
+          alert('Une erreur est survenue lors de l\'inscription. Veuillez réessayer.');
+        }
       } catch (error) {
-        // En cas d'erreur, vous pouvez afficher un message d'erreur ou prendre d'autres mesures
-        console.error("Erreur lors de l'inscription :", error);
-        alert("Une erreur est survenue lors de l'inscription. Veuillez réessayer.");
+        console.error('Erreur lors de l\'inscription :', error);
+        alert('Une erreur est survenue lors de l\'inscription. Veuillez réessayer.');
       }
     },
+    
     async login() {
       try {
         // Envoyer les données de connexion au serveur (simulé ici avec un délai)
@@ -55,36 +106,3 @@ export default {
 };
 </script>
 
-
-<template>
-  <v-app>
-    <v-main>
-      <v-container>
-        <v-card>
-          <v-card-title>
-            {{ isLogin ? 'Connexion' : 'Inscription' }}
-          </v-card-title>
-          <v-card-text>
-            <v-form v-if="!isLogin">
-              <v-text-field v-model="firstName" label="Prénom"></v-text-field>
-              <v-text-field v-model="lastName" label="Nom"></v-text-field>
-              <v-text-field v-model="email" label="Email"></v-text-field>
-              <v-text-field v-model="password" label="Mot de passe" type="password"></v-text-field>
-              <v-btn @click="register" color="primary">S'inscrire</v-btn>
-            </v-form>
-            <v-form v-else>
-              <v-text-field v-model="loginEmail" label="Email"></v-text-field>
-              <v-text-field v-model="loginPassword" label="Mot de passe" type="password"></v-text-field>
-              <v-btn @click="login" color="primary">Se connecter</v-btn>
-            </v-form>
-          </v-card-text>
-          <v-card-actions>
-            <v-btn @click="toggleLoginMode">
-              {{ isLogin ? "Pas encore inscrit ?" : "Déjà inscrit ?" }}
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-container>
-    </v-main>
-  </v-app>
-</template>
